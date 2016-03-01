58b41da254338206f92029889046d9e2938f73c9
Maciej Dziardziel <fiedzia@gmail.com>
2016-03-01 20:58:54 +0000

    pass CLIENT_OPTIONS to memcached client

--- django/core/cache/backends/memcached.py
+++ django/core/cache/backends/memcached.py
@@ -32,7 +32,12 @@ class BaseMemcachedCache(BaseCache):
         Implements transparent thread-safe access to a memcached client.
         """
         if getattr(self, '_client', None) is None:
-            self._client = self._lib.Client(self._servers)
+            if self._options and 'CLIENT_OPTIONS' in self._options:
+                client_options = dict(servers=self._servers)
+                client_options.update(self._options['CLIENT_OPTIONS'])
+                self._client = self._lib.Client(**client_options)
+            else:   
+                self._client = self._lib.Client(self._servers)
 
         return self._client
 
@@ -163,8 +168,12 @@ class MemcachedCache(BaseMemcachedCache):
     @property
     def _cache(self):
         if getattr(self, '_client', None) is None:
-            self._client = self._lib.Client(self._servers, pickleProtocol=pickle.HIGHEST_PROTOCOL,
-                    socket_timeout=self._options.get('socket_timeout', self._lib._SOCKET_TIMEOUT))
+            if self._options and 'CLIENT_OPTIONS' in self._options:
+                client_options = dict(servers=self._servers, pickleProtocol=pickle.HIGHEST_PROTOCOL)
+                client_options.update(self._options['CLIENT_OPTIONS'])
+                self._client = self._lib.Client(**client_options)
+            else:
+                self._client = self._lib.Client(self._servers, pickleProtocol=pickle.HIGHEST_PROTOCOL)
         return self._client
 
 
@@ -178,7 +187,12 @@ class PyLibMCCache(BaseMemcachedCache):
 
     @cached_property
     def _cache(self):
-        client = self._lib.Client(self._servers)
+        if self._options and 'CLIENT_OPTIONS' in self._options:
+            client_options = dict(servers=self._servers)
+            client_options.update(self._options['CLIENT_OPTIONS'])
+            client = self._lib.Client(**client_options)
+        else:
+            client = self._lib.Client(self._servers)
         if self._options:
             client.behaviors = self._options
 
